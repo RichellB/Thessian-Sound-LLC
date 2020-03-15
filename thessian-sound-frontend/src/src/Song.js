da0021583413017e4adce4d0cded68243a005090
richell <richell.bonfield12@gmail.com>
2020-03-15 11:02:40 -0500

    Added JS Rendering for Songs

--- thessian-sound-frontend/src/src/Song.js
+++ thessian-sound-frontend/src/src/Song.js
@@ -7,6 +7,33 @@ class Song {
         this.album = album 
         this.artistId = artistId 
 
+        this.element = document.createElement('div')
+        this.element.className = "song"
+        this.element.id = `song-${this.id}`
+
         Song.all.push(this)
     }
+
+    artist(){
+        return Artist.all.find(artist => artist.id === this.artistId)
+      }
+    
+      partialRender(){
+        this.element.innerHTML = `
+          <h3>${this.title}</h3>
+          <p>Album: ${this.album}</p>
+        `
+        return this.element
+      }
+    
+    
+      fullRender(){
+        this.element.innerHTML = `
+        <h1>${this.title}</h1>
+        <p>Album: ${this.album}</p>
+        <p>Artist: ${this.artist().name}</p>
+        `
+        return this.element
+      }
+    
 }
