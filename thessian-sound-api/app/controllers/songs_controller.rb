7f99dbee5e28b4b243750d92192e7af914b7766b
richell <richell.bonfield12@gmail.com>
2020-03-14 22:06:07 -0500

    Added JSON Rendering for songs

--- thessian-sound-api/app/controllers/songs_controller.rb
+++ thessian-sound-api/app/controllers/songs_controller.rb
@@ -1,52 +1,34 @@
 class SongsController < ApplicationController
 
     def index
-        @song = Song.find_by_id(params[:id])
-          if @artist = Artist.find_by_id(params[:artist_id])
-              @songs = @artist.songs.all
-          else
-              @songs = Song.all
-          end   
+       # @song = Song.find_by_id(params[:id])
+       #   if @artist = Artist.find_by_id(params[:artist_id])
+          #    @songs = @artist.songs.all
+         # else
+         #     @songs = Song.all
+         # end 
+         songs = Song.all
+         render json: SongSerializer.new(songs)  
   
-      end
-  
-      def new
-          if @artist = Artist.find_by_id(params[:artist_id])
-            @artist.songs = params[:song]  #Is this needed?
-            @song = @artist.songs.build
-              
-          else
-              @song = Song.new
-          end    
       end
   
       def create
-        @song = Song.new(song_params)
-        if @song.save
-            redirect_to songs_path
-        else
-              render :new
-          end
+        newSong = Song.create(title: params['title'], artist: params['artist'])
+        render json: SongSerializer.new(newSong)
       end
 
       def edit 
-        @song = Song.find_by_id(params[:id])
+        song = Song.find_by_id(params[:id])
       end
-
-      def update 
-        @song = Song.find_by_id(params[:id])
-        @song.update(title: params[:song][:title])
-        redirect_to song_path(@song)
-      end 
   
       def show
-          @song = Song.find_by_id(params[:id])
+          song = Song.find_by_id(params[:id])
+          render json: SongSerializer.new(song)
       end
 
       def destroy 
-        @song = Song.find_by_id(params[:id])
-        @song.destroy
-        redirect_to songs_path
+        song = Song.find_by_id(params[:id])
+        song.destroy
       end 
   
       private
