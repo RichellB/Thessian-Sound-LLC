308918d09304be554b12391d258769fb27e34b4b
richell <richell.bonfield12@gmail.com>
2020-03-15 17:43:59 -0500

    Created Song Migration

---
+++ thessian-sound-api/db/migrate/20200315214050_create_songs.rb
@@ -0,0 +1,12 @@
+class CreateSongs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :release_year
+      t.string :album
+      t.belongs_to :artist
+      t.belongs_to :user
+
+    end
+  end
+end
